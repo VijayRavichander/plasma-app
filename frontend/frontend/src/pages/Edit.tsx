bf537c6d81dff2ddf88357ebd43dee1ab137418f
Vijay Ravichander <vijaypreetham1@gmail.com>
2024-05-27 12:31:23 +0530

    Edit changes

--- frontend/frontend/src/pages/Edit.tsx
+++ frontend/frontend/src/pages/Edit.tsx
@@ -3,26 +3,40 @@ import { useState } from "react"
 import { AppBar } from "../components/AppBar"
 import axios from "axios"
 import { BACKEND_URL } from "../config"
-import { useNavigate, useParams } from "react-router-dom"
+import { useNavigate } from "react-router-dom"
+import { useEditor, EditorContent} from '@tiptap/react'
+import StarterKit from '@tiptap/starter-kit'
+import TipTapMenuBar from "../components/TipTapMenu"
+
 export const Edit = () => {
 
     const [title, setTitle] = useState("")
-    const [desc, setDesc] = useState("")
+    const [editorState, setEditorState] = useState("")
+    const [editorText, setEditorText] = useState("")
+
+    const editor = useEditor({
+        extensions: [StarterKit],
+        content: editorState,
+        onUpdate: ({editor}) => {
+          setEditorState(editor.getHTML());
+          setEditorText(editor.getText());
+        },
+      })
+
     const navigate = useNavigate()
-    const params = useParams();
     
     return <div>
         <AppBar />
         <div className="flex justify-center max-w-screen-xl w-screen">
             <div className="max-w-screen-lg w-full">
-                <div className="w-full my-2">
+                <div className="w-full my-2 mx-2">
                     <label className="block mb-2 text-md font-bold text-gray-900">Title</label>
                     <textarea onChange = {(e) => {
                         setTitle(e.target.value)
                     }}   
                     id="message" rows={1} className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border" placeholder="Title....."></textarea>
                 </div>
-                <div>
+                {/* <div>
                     <div className="bg-white rounded-b-lg w-full my-2">
                         <label  className="block mb-2 text-md font-bold text-gray-900">Your Story</label>
                         <textarea onChange = { (e) => {
@@ -30,13 +44,23 @@ export const Edit = () => {
                         }}
                         id="editor" rows={24} className="block w-full p-2.5 text-sm text-gray-800 bg-gray-50 border rounded-lg" placeholder="Write an article..." required ></textarea>
                     </div>
+                </div> */}
+                <div>
+
+                <div className='mx-2'>
+                    <div className="font-bold">Your Story</div>
+                        {editor && <TipTapMenuBar editor={editor} />}
+                        <div className='prose'>
+                            <EditorContent editor={editor} />
+                        </div>
+                    </div>
                 </div>
                 <button onClick = {
                     async () => {
                         const response = await axios.put(`${BACKEND_URL}/api/v1/blog`, {
                             title, 
-                            content: desc, 
-                            id: params.id
+                            content: editorText, 
+                            contentHMTL: editorState
                         }, {
                             headers: 
                             {
